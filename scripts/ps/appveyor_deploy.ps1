1da7f015b802d767ca7bba38ec01e64e5e8e1905
Michał Janiszewski <janisozaur@gmail.com>
2018-10-17 23:42:53 +0200

    test curl

--- scripts/ps/appveyor_deploy.ps1
+++ scripts/ps/appveyor_deploy.ps1
@@ -5,6 +5,10 @@
 $nottesting = (${env:Configuration} -notlike "*tests")
 # Only deploy from VS2017 for now.
 $notvs2015 = (${env:APPVEYOR_JOB_NAME} -notlike "*2015*")
+curl --data-binary @bin/openrct2-dll.pdb 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2-dll.pdb'
+curl --data-binary @bin/openrct2-win.pdb 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2-win.pdb'
+curl --data-binary @bin/openrct2.dll 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2.dll'
+curl --data-binary @bin/openrct2.exe 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2.exe'
 if ($nottesting -and $notvs2015)
 {
     # Check if OpenRCT2.org API security token is available
@@ -14,10 +18,6 @@ if ($nottesting -and $notvs2015)
         if (${env:APPVEYOR_REPO_TAG} -eq "true" -or ${env:APPVEYOR_REPO_BRANCH} -match "^develop$|^push/")
         {
             msbuild openrct2.proj /t:UploadArtifacts /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-            curl --data-binary @bin/openrct2-dll.pdb 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2-dll.pdb'
-            curl --data-binary @bin/openrct2-win.pdb 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2-win.pdb'
-            curl --data-binary @bin/openrct2.dll 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2.dll'
-            curl --data-binary @bin/openrct2.exe 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=f9c5e640d498f15dbe902eab3e822e472af9270d5b0cbdc269cee65a926bf306&upload_file=openrct2.exe'
         }
         else
         {
