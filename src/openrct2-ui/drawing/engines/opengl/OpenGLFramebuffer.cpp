acaa37d12a5590c70bad94048c70db6a562da901
Michał Janiszewski <janisozaur@gmail.com>
2020-10-15 22:29:13 +0200

    Replace functions missing in GLES

--- src/openrct2-ui/drawing/engines/opengl/OpenGLFramebuffer.cpp
+++ src/openrct2-ui/drawing/engines/opengl/OpenGLFramebuffer.cpp
@@ -92,7 +92,16 @@ void OpenGLFramebuffer::GetPixels(rct_drawpixelinfo& dpi) const
     auto pixels = std::make_unique<uint8_t[]>(_width * _height);
     glBindTexture(GL_TEXTURE_2D, _texture);
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
-    glGetTexImage(GL_TEXTURE_2D, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, pixels.get());
+    GLuint fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
+
+    glReadPixels(0, 0, _width, _height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.get());
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDeleteFramebuffers(1, &fbo);
+    //glGetTexImage(GL_TEXTURE_2D, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, pixels.get());
 
     // Flip pixels vertically on copy
     uint8_t* src = pixels.get() + ((_height - 1) * _width);
