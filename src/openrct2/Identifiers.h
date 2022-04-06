3fee43500ce3de65facb29309d633659ac7c3270
Michał Janiszewski <janisozaur@users.noreply.github.com>
2022-04-06 20:49:48 +0200

    Update Identifiers.h

--- src/openrct2/Identifiers.h
+++ src/openrct2/Identifiers.h
@@ -13,7 +13,7 @@
 #include "core/Identifier.hpp"
 
 #include <limits>
-
+#error here
 using BannerIndex = TIdentifier<uint16_t, std::numeric_limits<uint16_t>::max(), struct BannerIndexTag>;
 using ParkEntranceIndex = TIdentifier<uint8_t, std::numeric_limits<uint8_t>::max(), struct ParkEntranceIndexTag>;
 using RideId = TIdentifier<uint16_t, std::numeric_limits<uint16_t>::max(), struct RideIdTag>;
