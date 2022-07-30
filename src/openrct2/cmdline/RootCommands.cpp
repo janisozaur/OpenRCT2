1c88b5def190e1cdda7c791217dca3c735252cf9
Michał Janiszewski <janisozaur@gmail.com>
2022-07-30 22:59:23 +0200

    Provide more traces for breakpad

--- src/openrct2/cmdline/RootCommands.cpp
+++ src/openrct2/cmdline/RootCommands.cpp
@@ -459,6 +459,10 @@ static void PrintVersion()
     Console::WriteLine();
     Console::WriteFormat("Minimum park file version: %d", OpenRCT2::PARK_FILE_MIN_VERSION);
     Console::WriteLine();
+#ifdef USE_BREAKPAD
+    Console::WriteFormat("With breakpad support enabled");
+    Console::WriteLine();
+#endif
 }
 
 static void PrintLaunchInformation()
