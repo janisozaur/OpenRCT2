57a5efc1ebc3a9fa4fbfb696d66689e965853a01
Michał Janiszewski <janisozaur@gmail.com>
2023-05-09 13:06:33 +0200

    Provide single place for setting desired FPS, skip rendering of final frame

--- src/openrct2/Context.cpp
+++ src/openrct2/Context.cpp
@@ -73,6 +73,9 @@
 #include "scenes/title/TitleSequenceManager.h"
 #include "scripting/HookEngine.h"
 #include "scripting/ScriptEngine.h"
+#include "scenes/title/TitleSequence.h"
+#include "scenes/title/TitleSequenceManager.h"
+#include "scenes/title/TitleSequenceRender.h"
 #include "ui/UiContext.h"
 #include "ui/WindowManager.h"
 #include "world/MapAnimation.h"
@@ -1328,7 +1331,7 @@ namespace OpenRCT2
                 tweener.Reset();
             }
 
-            constexpr float deltaTime = 1.0f / 60;
+            constexpr float deltaTime = 1.0f / FPS;
 
             UpdateTimeAccumulators(deltaTime);
 
