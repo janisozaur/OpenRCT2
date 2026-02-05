338a0f9aefaaff0410ef507abaf09a22e6ad9a08
Michał Janiszewski <janisozaur@gmail.com>
2026-02-05 19:16:10 +0100

    Fixes

--- src/openrct2/core/File.cpp
+++ src/openrct2/core/File.cpp
@@ -33,7 +33,7 @@ namespace OpenRCT2::File
     bool Exists(u8string_view path)
     {
 #ifdef __ANDROID__
-        if (OpenRCT2::::OpenRCT2::String::startsWith(path, "/android_asset/"))
+        if (OpenRCT2::String::startsWith(path, "/android_asset/"))
         {
             auto assetManager = static_cast<AAssetManager*>(::OpenRCT2::Platform::GetAssetManager());
             if (assetManager != nullptr)
