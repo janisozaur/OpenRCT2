84fe38854848fa8d35582d2edd69a4be9b457265
Michał Janiszewski <janisozaur@gmail.com>
2026-02-05 17:02:56 +0100

    Format sources

--- src/openrct2/platform/Platform.Android.cpp
+++ src/openrct2/platform/Platform.Android.cpp
@@ -175,9 +175,7 @@ namespace OpenRCT2::Platform
         }
 
         uint64_t lastModified = 0;
-        struct stat statInfo
-        {
-        };
+        struct stat statInfo{};
         if (stat(std::string(path).c_str(), &statInfo) == 0)
         {
             lastModified = statInfo.st_mtime;
@@ -205,9 +203,7 @@ namespace OpenRCT2::Platform
         }
 
         uint64_t size = 0;
-        struct stat statInfo
-        {
-        };
+        struct stat statInfo{};
         if (stat(std::string(path).c_str(), &statInfo) == 0)
         {
             size = statInfo.st_size;
