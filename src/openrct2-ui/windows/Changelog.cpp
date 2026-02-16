91119159ecf8471910347d9e6e7a6b15c0285d77
Michał Janiszewski <janisozaur@gmail.com>
2026-02-16 19:03:15 +0100

    Fixup changelog

--- src/openrct2-ui/windows/Changelog.cpp
+++ src/openrct2-ui/windows/Changelog.cpp
@@ -14,6 +14,7 @@
 #include <openrct2/OpenRCT2.h>
 #include <openrct2/PlatformEnvironment.h>
 #include <openrct2/Version.h>
+#include <openrct2/core/File.h>
 #include <openrct2/core/FileSystem.hpp>
 #include <openrct2/core/String.hpp>
 #include <openrct2/drawing/Drawing.h>
@@ -65,18 +66,7 @@ namespace OpenRCT2::Ui::Windows
         {
             auto& env = GetContext()->GetPlatformEnvironment();
             auto path = env.GetFilePath(pathId);
-            auto fs = std::ifstream(fs::u8path(path), std::ios::in);
-            if (!fs.is_open())
-            {
-                throw std::runtime_error("Unable to open " + path);
-            }
-            fs.seekg(0, fs.end);
-            auto length = fs.tellg();
-            fs.seekg(0, fs.beg);
-            std::unique_ptr<char[]> buffer = std::make_unique<char[]>(length);
-            fs.read(buffer.get(), length);
-            auto result = std::string(buffer.get(), buffer.get() + length);
-            return result;
+            return File::ReadAllText(path);
         }
 
         /**
