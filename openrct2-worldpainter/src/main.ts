b56416e860072321ed8318aebc7a581fed25cd7b
Michał Janiszewski <janisozaur@gmail.com>
2026-03-28 20:27:48 +0100

    Update path

--- openrct2-worldpainter/src/main.ts
+++ openrct2-worldpainter/src/main.ts
@@ -5,7 +5,7 @@
 * under the GNU General Public License version 3.
 *****************************************************************************/
 
-/// <reference path="../../OpenRCT2/distribution/openrct2.d.ts" />
+/// <reference path="../../distribution/openrct2.d.ts" />
 
 import * as TerrainManager from "./TerrainManager";
 import * as Tools from "./Tools";
