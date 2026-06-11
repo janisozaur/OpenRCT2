36592ed3421e8cf4033b6f9dc14a8d9d14fac928
Michał Janiszewski <janisozaur@gmail.com>
2026-06-11 10:37:36 +0200

    Optimize s

--- src/openrct2/drawing/TTFSDLPort.cpp
+++ src/openrct2/drawing/TTFSDLPort.cpp
@@ -575,6 +575,11 @@ static void Flush_Cache(TTF_Font* font)
     }
 }
 
+#if defined(_MSC_VER) && _MSC_VER == 1951
+// MSVC bug in 19.51, using different optimization setting fixes the codegen
+// see https://github.com/OpenRCT2/OpenRCT2/issues/26637
+#pragma optimize("s", on)
+#endif
 static FT_Error Load_Glyph(TTF_Font* font, uint16_t ch, c_glyph* cached, int want)
 {
     FT_Face face;
